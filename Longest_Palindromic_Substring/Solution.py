999e059ef4871d2c32d62015902f4f61111d0097
ReeceKalmar <ralmarkalmar@gmail.com>
2024-12-26 03:33:20 -0700

    crazy, i was crazy once...

--- Longest_Palindromic_Substring/Solution.py
+++ Longest_Palindromic_Substring/Solution.py
@@ -1,26 +1,44 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        if len(s) <= 1:
+        if len(s) == 1:
             return s
+        
+        longest = [0, 0]
+        for i in range (len(s)):
+            l = i
+            r = len(s) - 1
+            rtemp = r
+            temp = []
+
+            while l < r:
+                if s[l] != s[r]:
+                    l = i
+                    temp = []
+                    rtemp -= 1
+                    r = rtemp
+                    continue
+                
+                if len(temp) == 0:
+                    temp = [l, r]
+                l += 1
+                r -=1
+            if len(temp) > 0 and temp[1] - temp[0] > longest[1] - longest[0]:
+                longest = temp
+
+        if longest[0] != 0 or longest[1] != 0:
+            return s[longest[0] : longest[1] + 1]
+
         longest = ""
-        t = ""
-
-        # case when theres only length 2 palindrome
-        for i in range(len(s)):
-            if i < len(s) - 1 and s[i] == s[i + 1]:
-                longest = s[i] * 2
-
-        for i in range(1, len(s) - 1):
-            if s[i - 1] == s[i + 1]:
-                t = s[i - 1 : i + 2]
-                left = i - 2
-                right = i + 2
-                while left >= 0 and right < len(s):
-                    if s[left] == s[right]:
-                        t = s[left] + s[right]
-                    else:
-                        break
-                if len(t) > len(longest):
-                    longest = t
-
+        temp = ""
+        previous = s[0]
+        for char in s:
+            if char == previous:
+                temp += char
+            else:
+                if len(temp) > len(longest):
+                    longest = temp
+                temp = char
+                previous = char
+        if len(temp) > len(longest):
+            longest = temp
         return longest
